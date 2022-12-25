57882f6170dceb26abad90b7b280728437cd542f
Garrett Hale <gthkeno@gmail.com>
2022-12-25 12:20:50 -0600

    regular game mode

--- src/gamemode/Gamemode.zig
+++ src/gamemode/Gamemode.zig
@@ -35,8 +35,8 @@ pub const GameModes = union(enum) {
                 const ptr = @ptrCast(*Cutscene, self);
                 if (ptr.update(controller)) {
                     self.* = switch (ptr.from) {
-                        //.main_menu => GameModes{ .labyrinth = Labyrinth{} },
-                        .main_menu => GameModes{ .boss_fight = BossFight{} },
+                        .main_menu => GameModes{ .labyrinth = Labyrinth{} },
+                        //.main_menu => GameModes{ .boss_fight = BossFight{} },
                         .labyrinth => GameModes{ .boss_fight = BossFight{} },
                         .boss_fight => GameModes{ .main_menu = MainMenu{} },
                     };
